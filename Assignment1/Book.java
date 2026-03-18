86b0743a0948f4f5acfebc945bd941e40fb6cf25
Mateo Grueso <mateogrueso10@gmail.com>
2026-03-18 13:17:36 -0400

    default constructor set

---
+++ Assignment1/Book.java
@@ -0,0 +1,33 @@
+package Assignment1;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean available;
+
+    // Default constructor
+    public Book() {
+        this.title = "Unknown";
+        this.author = "Unknown";
+        this.isbn = "0000000000";
+        this.available = true;
+    }
+
+    // Parameterized constructor
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = true;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+}
